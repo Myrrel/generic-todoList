74d845f1f02167d173f36c558024134e1e2b8c94
Martín Urciuoli <martin.urciuoli@outlook.com>
2023-11-11 11:02:18 -0300

    Initial Commit

---
+++ generic-todoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  generic-todoList
+//
+//  Created by Harlock on 11/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ generic-todoList/generic_todoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  generic_todoListApp.swift
+//  generic-todoList
+//
+//  Created by Harlock on 11/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct generic_todoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
